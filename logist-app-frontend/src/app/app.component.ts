ce395abe49f39466288f92ea29b98bbba6468a51
Alex Kulikov <aleksey.kulikov.96@gmail.com>
2023-06-23 18:01:50 +0300

    создание на фронте и бэке методов для тестирования связи сервисов фронта и бэкенда через нжинкс

--- logist-app-frontend/src/app/app.component.ts
+++ logist-app-frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'logist-app-frontend';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  makeRequest1(): void {
+    this.httpClient.get('https://pokeapi.co/api/v2/pokemon/ditto').subscribe(v => console.log(v), e => console.log(e))
+  }
+
+  makeRequestToApi(): void {
+    this.httpClient.get('/api/users/ping').subscribe(v => console.log(v), e => console.log(e))
+  }
 }
